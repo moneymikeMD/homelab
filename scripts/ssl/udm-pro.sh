554b3261897b15ddf325785c163a1149d0885b72
Mike Garrett <mike.garrett@motorefi.com>
2024-12-15 11:19:28 -0500

    Make the script work without compromising sudo

--- scripts/ssl/udm-pro.sh
+++ scripts/ssl/udm-pro.sh
@@ -1,6 +1,17 @@
 # A script to update the SSL certs on my UDM Pro
 # Adapted from https://github.com/ScottHelme/ubiquiti-https-scripts/blob/main/udm-pro.sh
 # 1.139 being docker-host; 1.1 being UDM
-scp mike@10.100.1.139:/certs/home.dipuce.com/fullchain.cer root@10.100.1.1:/data/unifi-core/config/unifi-core.crt
-scp mike@10.100.1.139:/certs/home.dipuce.com/home.dipuce.com.key root@10.100.1.1:/data/unifi-core/config/unifi-core.key
+
+# Grab cert files and copy locally
+echo "Grabbing files from Docker host"
+ssh mike@10.100.1.139 "sudo -S cat /certs/home.dipuce.com/fullchain.cer" > ./fullchain.cer
+ssh mike@10.100.1.139 "sudo -S cat /certs/home.dipuce.com/home.dipuce.com.key" > ./dipuce.key
+
+echo "Transfering files to UDM Pro"
+scp fullchain.cer root@10.100.1.1:/data/unifi-core/config/unifi-core.crt
+scp dipuce.key root@10.100.1.1:/data/unifi-core/config/unifi-core.key
 ssh root@10.100.1.1 'systemctl restart unifi-core'
+
+echo "Removing copied files"
+rm fullchain.cer
+rm dipuce.key
