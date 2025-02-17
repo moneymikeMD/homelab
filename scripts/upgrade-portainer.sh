c48328cdb282d1342db29a1fc48e5c883d8bf9d8
Mike Garrett <mike.garrett@motorefi.com>
2025-02-17 17:59:29 -0500

    Update Portainer scripts

--- scripts/upgrade-portainer.sh
+++ scripts/upgrade-portainer.sh
@@ -4,15 +4,17 @@ docker stop portainer
 docker rm portainer
 
 # Pull new container
-docker pull portainer/portainer-ee:2.21.4
+docker pull portainer/portainer-ee
 
 # Run a new container
-docker run -d -p 8000:8000 \
+docker run -d \
+-p 8000:8000 \
 -p 9443:9443 \
 --name=portainer \
 --restart=always \
+--sslcert /etc/user-ssl/fullchain.cer \
+--sslkey /etc/user-ssl/app.dipuce.com.key \
 -v /var/run/docker.sock:/var/run/docker.sock \
--v portainer_data:/data portainer/portainer-ee:2.21.4 \
--v /certs/app.dipuce.com:/etc/ssl \
---sslcert /etc/ssl/fullchain.cer \
---sslkey /etc/ssl/app.dipuce.com.key
+-v portainer_data:/data \
+-v /certs/app.dipuce.com:/etc/user-ssl \
+portainer/portainer-ee
